66b05491f1855be25142408c6e92b24d3bbf3b2b
retroric <retroric48k@yahoo.com>
2019-03-03 20:51:02 +0100

    osdk_config.bat: Added OSDKCOMP=-O3 compilation option, set OSDKNAME=MovingMap

--- osdk_config.bat
+++ osdk_config.bat
@@ -4,5 +4,8 @@
 :: Set the build paremeters
 ::
 SET OSDKADDR=$800
-SET OSDKNAME=MAIN
-SET OSDKFILE=MAIN
+SET OSDKNAME=MovingMap
+:: Set OISDK Compilation option (-O2=standard optimization, -O3=aggressive optimization)
+SET OSDKCOMP=-O3
+::
+SET OSDKFILE=main
